a90f5b78e1b06660b3855b9e51bd57c5c13e32c6
shashi699 <shashissk228@gmail.com>
2026-07-31 12:54:15 +0530

    Initial Spring Boot project

---
+++ distributed-file-system/src/main/java/com/shashi/distributedfilesystem/DistributedFileSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shashi.distributedfilesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedFileSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedFileSystemApplication.class, args);
+    }
+
+}
---
+++ distributed-file-system/src/test/java/com/shashi/distributedfilesystem/DistributedFileSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.distributedfilesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedFileSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
